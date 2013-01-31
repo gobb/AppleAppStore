edfba989213d6fda5fc6f098f27c87ae80692b0e
Vitaliy Zhuk <zhuk2205@gmail.com>
2013-01-31 09:58:12 +0000

    Add search component

---
+++ src/Apple/AppStore/AppStores.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the AppleAppStore package
+ *
+ * (c) Vitaliy Zhuk <zhuk2205@gmail.com>
+ *
+ * For the full copyring and license information, please view the LICENSE
+ * file that was distributed with this source code
+ */
+
+namespace Apple\AppStore;
+
+/**
+ * Class for search app stores
+ */
+class AppStores
+{
+    /**
+     * Search app store by country ISO
+     *
+     * @param string $countryISO
+     * @param boolean $initialize
+     */
+    static public function getAppStoreByCountry($countryISO, $initialize = TRUE)
+    {
+        $countryISO = strtoupper($countryISO);
+
+        $classAppStore = 'Apple\\AppStore\\Stores\\' . $countryISO . 'Store';
+
+        if (!class_exists($classAppStore)) {
+            throw new \InvalidArgumentException(sprintf(
+                'Not found app store by country key: "%s".',
+                $countryISO
+            ));
+        }
+
+        return $initialize ? new $classAppStore : $classAppStore;
+    }
+}
